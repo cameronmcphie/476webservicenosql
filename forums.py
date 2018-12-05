d47081b8e3d88fd35591bdd488498af300540b30
cameronmcphie <quiksilver980@gmail.com>
2018-12-04 21:57:59 -0800

    POST is implemented and working

--- forums.py
+++ forums.py
@@ -1,5 +1,6 @@
 from helper_funs import *
 from cassandra.cluster import Cluster
+from uuid import uuid4, UUID
 
 
 def forum():
@@ -8,54 +9,35 @@ def forum():
         # auth contains the username and Password
         auth = request.authorization
 
+        # TODO: uncomment when working 
         if auth_check(auth) is False:
             return get_response(401)
 
+        # Get and parse the forum name from the POST
         forum_submit = request.get_json()
-        #parse the name from JSON
         forum_name = forum_submit.get('name')
-        
-        # Possible implementation for GET and POST of forums
-        # forums
-        # POST
-        # INSERT INTO discussion_forum.Content (forumid, forumcreator, forumtitle, postid, postauthor, posttimestamp, posttext)
-        # VALUES (?, ?, ?, ?, ?, ?, ?)
 
-        # forum_post_stmt = session.prepare("
-        #                   INSERT INTO discussion_forum.Content (forumid, forumcreator, forumtitle, postid, postauthor, posttimestamp, posttext)
-        #                   VALUES (?, ?, ?, ?, ?, ?, ?)
-        #               ")
+        cluster = Cluster(['172.17.0.1 ','172.17.0.2'])
 
-        # GET
-        # SELECT 		forumid, forumcreator, forumtitle, posttimestamp 
-        # FROM 		discussion_forum.Content 
-        # WHERE 		forumid=?;
+        session = cluster.connect("discussion_forum")
 
-        # forum_get_stmt = session.prepare("SELECT forumid, forumcreator, forumtitle, posttimestamp FROM discussion_forum.Content where forumid=? ORDER BY posttimestamp DESC")
+        check_forum_title = session.execute("SELECT forumtitle FROM Forums where forumtitle = %s;", [forum_name])
+        # Check if forum already exists
+        forums = {}
+        for forum_row in check_forum_title:
+            forums['id'] = forum_row.forumtitle
+        if(not bool(forums)):
+            new_forum_id = uuid4()
+            creator = auth['username']
 
-        
-        # If forumn name does't exist insert it into the db and return success
-        if query_db('SELECT ForumsName from Forums where ForumsName = ?', [request.get_json().get('name')], one=True) is None:
-            query = 'INSERT into Forums (CreatorId, ForumsName) Values ((Select UserId from Users where Username = ?), ?);'
-            conn = get_db()
-            cur = conn.cursor()
-            cur.execute(query, (auth.username, str(forum_name)))
-            forum = cur.execute('SELECT last_insert_rowid() as ForumId;').fetchall()
-            forumid = dict(forum[0]).get('ForumId')
-            conn.commit()
-            conn.close()
-            return get_response(201, body=None, location=('/forums/'+str(forumid)))
+            session.execute("INSERT INTO Forums (forumid, forumcreator, forumtitle) VALUES (%s, %s, %s);", [new_forum_id, creator, forum_name])
+
+            return get_response(201, body=None, location=('/forums/'+str(new_forum_id)))
         else:
             return get_response(409)
+
     #request for all the present forums
-    elif request.method == 'GET':
-        
-        # query = 'SELECT Users.Username as creator, Forums.ForumId as id, Forums.ForumsName as name FROM Forums, Users where CreatorId = UserId;'
-        # conn = sqlite3.connect(DATABASE)
-        # conn.row_factory = dict_factory
-        # cur = conn.cursor()
-        # all_forums = cur.execute(query).fetchall()
-        
+    elif request.method == 'GET':        
         cluster = Cluster(['172.17.0.1 ','172.17.0.2'])
 
         session = cluster.connect("discussion_forum")
