a9e1ec32a2e995c389c851192dc5b64da8675386
Elmer Sevillano <esevillano@ymail.com>
2018-12-04 21:10:29 -0800

    POST method working for posts

--- posts.py
+++ posts.py
@@ -1,6 +1,8 @@
 from helper_funs import *
+from datetime import datetime
 from cassandra.cluster import Cluster
-from uuid import UUID
+from cassandra import util
+from uuid import uuid4, UUID
 
 def post(forum_id, thread_id):
     if request.method == 'POST':
@@ -9,20 +11,44 @@ def post(forum_id, thread_id):
             return get_response(401)
 
         if forum_id and thread_id:
-            checkifforumexists = query_db('SELECT 1 from Forums where ForumId = ?;', [forum_id])
-            checkifthreadexists = query_db('SELECT 1 from Threads where ThreadId = ?;', [thread_id])
-            if (checkifforumexists == []) or (checkifthreadexists == []):
-                return get_response(404)
+            # checkifforumexists = query_db('SELECT 1 from Forums where ForumId = ?;', [forum_id])
+            # checkifthreadexists = query_db('SELECT 1 from Threads where ThreadId = ?;', [thread_id])
+            # if (checkifforumexists == []) or (checkifthreadexists == []):
+            #     return get_response(404)
+            #
+            # user = query_db('SELECT UserId from Users where Username = ?;', [auth.username])
+            # userid = dict(user[0]).get('UserId')
+            # requestJSON = request.get_json()
+            # timestamp = strftime('%a, %d %b %Y %H:%M:%S', gmtime()) + ' GMT'
+            # conn = get_db()
+            # cur = conn.cursor()
+            # cur.execute('INSERT into Posts (`AuthorId`, `ThreadBelongsTo`, `PostsTimestamp`, `Message`) values (?,?,?,?);', (userid, thread_id, timestamp, requestJSON.get('text')))
+            # conn.commit()
+            # conn.close()
 
-            user = query_db('SELECT UserId from Users where Username = ?;', [auth.username])
-            userid = dict(user[0]).get('UserId')
-            requestJSON = request.get_json()
-            timestamp = strftime('%a, %d %b %Y %H:%M:%S', gmtime()) + ' GMT'
-            conn = get_db()
-            cur = conn.cursor()
-            cur.execute('INSERT into Posts (`AuthorId`, `ThreadBelongsTo`, `PostsTimestamp`, `Message`) values (?,?,?,?);', (userid, thread_id, timestamp, requestJSON.get('text')))
-            conn.commit()
-            conn.close()
+            cluster = Cluster(['172.17.0.1 ','172.17.0.2'])
+            session = cluster.connect("discussion_forum")
+
+            author = auth['username']
+
+            # author = author.replace('"', "'")
+
+            data = request.get_json()
+            post_text = data.get("text")
+            # post_text = post_text.replace('"', "'")
+
+            threadtitles = session.execute("SELECT threadtitle from Content WHERE forumid=%s and threadid=%s ALLOW FILTERING", [UUID(forum_id), UUID(thread_id)])
+
+            for m in threadtitles:
+                thread_title = m[0]
+
+            print(thread_title)
+            # thread_title = threadtitles[0]
+            # print(theadtitles['threadtitle'])
+
+            # thread_title = thread_title.replace('"', "'")
+
+            session.execute("INSERT INTO Content (postid, forumid, postauthor, posttext, posttimestamp, threadid, threadtitle) VALUES(%s, %s, %s, %s, %s, %s, %s)", (uuid4(), UUID(forum_id), author, post_text, datetime.utcnow(), UUID(thread_id), thread_title))
 
             return get_response(201)
         else:
